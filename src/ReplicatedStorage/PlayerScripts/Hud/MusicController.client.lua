6655f9b34adcc6ffb4ad05b1fcd7f61b4d2d51cd
Netsu <58432847+netssu@users.noreply.github.com>
2026-02-14 12:31:43 -0300

    b

--- src/ReplicatedStorage/PlayerScripts/Hud/MusicController.client.lua
+++
@@ -1,9 +0,0 @@
--- SERVICES
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
--- CONSTANTS
-local SoundController = require(ReplicatedStorage.Modules.Utility.SoundUtility)
-local SoundData = require(ReplicatedStorage.Modules.Datas.SoundData)
-
--- INIT
-SoundController.PlayMusic(SoundData.Music.MainMenu, true, true)
